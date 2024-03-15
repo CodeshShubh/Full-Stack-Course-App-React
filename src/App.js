e796b7f3b611b9211327832a2749b4cf7cf61a22
Shubhanshu Saadhiyaan <codeshshubh@gmail.com>
2024-03-15 18:29:54 +0530

    home page frontend work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router , Route , Routes} from "react-router-dom";
-import Home from './components/Home.jsx/Home';
+import Home from './components/Home/Home';
 
 function App() {
   return (
