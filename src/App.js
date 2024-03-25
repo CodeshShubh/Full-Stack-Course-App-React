9f9b63a696691f16cb73e68da39a5a8627809f6a
Shubhanshu Saadhiyaan <codeshshubh@gmail.com>
2024-03-26 03:26:14 +0530

    About, ForgetPassword, resetpassword, contact, requesr

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import Courses from './components/Courses/Courses';
 import Footer from './components/Layout/Footer/Footer'
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
+import ForgetPassword from './components/Auth/ForgetPassword';
+import ResetPassword from './components/Auth/ResetPassword';
+import Contact from './components/Contact/Contact';
+import Request from './components/Request/Request';
+import About from './components/About/About';
 
 function App() {
   return (
@@ -14,8 +19,13 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/Courses' element={<Courses/>}/>
+        <Route path='/Contact' element={<Contact/> }/>
+        <Route path='/request' element={<Request/> }/>
+        <Route path='/about' element={<About/> }/>
         <Route path='/login' element={<Login/>} />
         <Route path='/Register' element={<Register/> } />
+        <Route path='/forgetpassword' element={<ForgetPassword/> } />
+        <Route path='/resetpassword/:token' element={<ResetPassword/> } />
       </Routes>
       <Footer/>
     </Router>
